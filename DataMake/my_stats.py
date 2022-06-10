e9059aa5b6a350842637fee0a3de0441625ddacb
Kyohei Uemura <kyoehi1995@icloud.com>
2022-06-11 00:19:50 +0900

    stats

--- DataMake/my_stats.py
+++ DataMake/my_stats.py
@@ -10,45 +10,55 @@ with open(filename,encoding='utf-8') as f:
 	stats_title = next(reader)
 	header_row = next(reader)
 
-	for row in reader:
-		print(row)
+	# for row in reader:
+	# 	print(row)
 
 	# for index,column_header in enumerate(header_row):
 	# 	print(index,column_header)
 
 
 
-	dates,weight = [],[]
+	dates,weights = [],[]
 	for row in reader:
-		current_date = datetime.strptime(row[2], '%Y-%m-%d')
+		current_date = datetime.strptime(row[0], '%Y/%m/%d')
+		current_weight = row[1]
+
+		# print(current_date,current_weight)
+
+		# if current_weight is None:
+
+		try:
+			w = float(current_weight)
+
+		except ValueError:
+			print(f"Missing data for {current_date}")
 		
-# 		try:
-# 			high = int(row[4])
-# 			low = int(row[5])
-# 		except ValueError:
-# 			print(f"Missing data for {current_date}")
-# 		else:
-# 			dates.append(current_date)
-# 			highs.append(high)
-# 			lows.append(low)
-	
-# # 	# print(highs)
+		else:
+
+			weights.append(w)
+			dates.append(current_date)
+
+			print(w)
+
+	print(dates,weights)
 
 
-# 	# for index , column_header in enumerate(header_row):
-# 	# 	print(index,column_header)
 
 
-# plt.style.use('seaborn')
-# fig,ax = plt.subplots()
+	# for index , column_header in enumerate(header_row):
+ 	# 	print(index,column_header)
+
+
+plt.style.use('seaborn')
+fig,ax = plt.subplots()
 # ax.plot(dates,highs,c='red')
 # ax.plot(dates,lows,c='blue')
-# plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
+plt.fill_between(dates,weights,facecolor='blue')
 
-# plt.title("Daily high and low temperatures,July 2018",fontsize = 24)
-# plt.xlabel('',fontsize=16)
-# fig.autofmt_xdate()
-# plt.ylabel("Temperature (F)",fontsize=16)
-# plt.tick_params(axis='both',which='major',labelsize=16)
+plt.title("stats_title",fontsize = 24)
+plt.xlabel('',fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)",fontsize=16)
+plt.tick_params(axis='both',which='major',labelsize=16)
 
-# plt.show()
+plt.show()
