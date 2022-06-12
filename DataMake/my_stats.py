dcd59c9c0de924e28c3958f0adc76a02322532ab
Kyohei Uemura <kyoehi1995@icloud.com>
2022-06-12 23:50:20 +0900

    stats

--- DataMake/my_stats.py
+++ DataMake/my_stats.py
@@ -4,28 +4,18 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 filename = 'stats\MyStats_Data_Daily_2021_5-2022_5.csv'
+filename_nippou = 'stats\nippou\日報素データ_20220501_20220531.csv'
+
 
 with open(filename,encoding='utf-8') as f:
 	reader =csv.reader(f)
-	stats_title = next(reader)
+	stats_title = next(reader)[0]
 	header_row = next(reader)
-
-	# for row in reader:
-	# 	print(row)
-
-	# for index,column_header in enumerate(header_row):
-	# 	print(index,column_header)
-
-
 
 	dates,weights = [],[]
 	for row in reader:
 		current_date = datetime.strptime(row[0], '%Y/%m/%d')
 		current_weight = row[1]
-
-		# print(current_date,current_weight)
-
-		# if current_weight is None:
 
 		try:
 			w = float(current_weight)
@@ -45,17 +35,18 @@ with open(filename,encoding='utf-8') as f:
 
 
 
-	# for index , column_header in enumerate(header_row):
- 	# 	print(index,column_header)
+
+
 
 
 plt.style.use('seaborn')
 fig,ax = plt.subplots()
 # ax.plot(dates,highs,c='red')
 # ax.plot(dates,lows,c='blue')
-plt.fill_between(dates,weights,facecolor='blue')
+# plt.fill_between(dates,weights,facecolor='blue')
+plt.plot(dates,weights,linewidth=3)
 
-plt.title("stats_title",fontsize = 24)
+plt.title(stats_title,fontsize = 24)
 plt.xlabel('',fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)",fontsize=16)
